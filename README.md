49f16d89ae80129ba2481b3fdb1fc67959581c4b
Remigiusz Kollataj <remigiusz.kollataj@gmail.com>
2026-02-17 08:43:22 +0100

    Add support for HTTP port
    Signed-off-by: Remigiusz Kollataj <remigiusz.kollataj@gmail.com>

--- README.md
+++ README.md
@@ -63,7 +63,13 @@ docker compose pull
 docker compose up
 ```
 
-Iris shall be available on the host interface, port 443, protocol HTTPS - ``https://<your_instance_ip>/iris``.  
+Iris protocol exposure is controlled by listening port variables:
+- ``INTERFACE_HTTP_PORT`` enables HTTP (example: ``http://<your_instance_ip>/``)
+- ``INTERFACE_HTTPS_PORT`` enables HTTPS (example: ``https://<your_instance_ip>/``)
+
+If one of these variables is unset or empty, the corresponding protocol is disabled in Nginx.  
+If both are unset, Nginx will not start.  
+If ``IRIS_BASE_PATH`` is set, prepend it to the URL (for example ``/iris`` -> ``https://<your_instance_ip>/iris``).  
 By default, an ``administrator`` account is created. The password is printed in stdout the very first time Iris is started. It won't be printed anymore after that.  
 ``WARNING :: post_init :: create_safe_admin :: >>>`` can be searched in the logs of the `webapp` docker to find the password.  
 The initial password can be set via the [configuration](https://docs.dfir-iris.org/operations/configuration/).   
@@ -113,4 +119,3 @@ The contents of this repository is available under [LGPL3 license](LICENSE.txt).
 
 ## Sponsoring
 Special thanks to Deutsche Telekom Security GmbH for sponsoring us!
-
--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -20,9 +20,44 @@
 
 set -e
 
+HTTP_LISTEN_DIRECTIVE=""
+HTTPS_LISTEN_DIRECTIVE=""
+SSL_CONFIG_BLOCK=""
+
+if [ -n "${INTERFACE_HTTP_PORT:-}" ]; then
+  HTTP_LISTEN_DIRECTIVE="listen          ${INTERFACE_HTTP_PORT};"
+fi
+
+if [ -n "${INTERFACE_HTTPS_PORT:-}" ]; then
+  if [ -z "${CERT_FILENAME:-}" ] || [ -z "${KEY_FILENAME:-}" ]; then
+    echo "INTERFACE_HTTPS_PORT is set but CERT_FILENAME or KEY_FILENAME is missing" >&2
+    exit 1
+  fi
+  HTTPS_LISTEN_DIRECTIVE="listen          ${INTERFACE_HTTPS_PORT} ssl;"
+  SSL_CONFIG_BLOCK="    # SSL CONF, STRONG CIPHERS ONLY
+    ssl_protocols               TLSv1.2 TLSv1.3;
+
+    ssl_prefer_server_ciphers   on;
+    ssl_certificate             /www/certs/${CERT_FILENAME};
+    ssl_certificate_key         /www/certs/${KEY_FILENAME};
+    ssl_ecdh_curve              secp521r1:secp384r1:prime256v1;
+    ssl_buffer_size             4k;
+
+    # DISABLE SSL SESSION CACHE
+    ssl_session_tickets         off;
+    ssl_session_cache           none;"
+fi
+
+if [ -z "${HTTP_LISTEN_DIRECTIVE}" ] && [ -z "${HTTPS_LISTEN_DIRECTIVE}" ]; then
+  echo "No interface port set. Set INTERFACE_HTTP_PORT and/or INTERFACE_HTTPS_PORT." >&2
+  exit 1
+fi
+
+export HTTP_LISTEN_DIRECTIVE HTTPS_LISTEN_DIRECTIVE SSL_CONFIG_BLOCK
+
 # envsubst will make a substitution on every $variable in a file, since the nginx file contains nginx variable like $host, we have to limit the substitution to this set
 # otherwise, each nginx variable will be replaced by an empty string
-envsubst '${INTERFACE_HTTPS_PORT} ${IRIS_UPSTREAM_SERVER} ${IRIS_UPSTREAM_PORT} ${SERVER_NAME} ${KEY_FILENAME} ${CERT_FILENAME} ${IRIS_BASE_PATH}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
+envsubst '${HTTP_LISTEN_DIRECTIVE} ${HTTPS_LISTEN_DIRECTIVE} ${SSL_CONFIG_BLOCK} ${IRIS_UPSTREAM_SERVER} ${IRIS_UPSTREAM_PORT} ${SERVER_NAME} ${KEY_FILENAME} ${CERT_FILENAME} ${IRIS_BASE_PATH}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
 cp /tmp/nginx.conf /etc/nginx/nginx.conf
 rm /tmp/nginx.conf
 
